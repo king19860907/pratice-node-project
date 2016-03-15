e59a1cd42c84e92706bd0f916fabdc780ed9f8fa
jun_ma <ludy19860907@163.com>
2016-03-15 15:31:40 +0800

    初始导入

---
+++ src/server.js
@@ -0,0 +1,30 @@
+'use strcit'
+/**
+ * Created by jun_ma on 2016/3/15.
+ */
+
+import path from 'path';
+import ProjectCore from 'project-core';
+
+const $ = new ProjectCore();
+
+//加载配置文件
+$.init.add((done)=>{
+    $.config.load(path.resolve(__dirname,'config.js'));
+    const env = process.env.NODE_ENV || null;
+    if(env){
+        $.config.load(path.resolve(__dirname,'../config',env+'.js'));
+    }
+    $.env = env;
+    done();
+});
+
+//初始化
+$.init((err)=>{
+    if(err){
+        console.error(err);
+        process.exit(-1);
+    }else{
+        console.log('inited');
+    }
+});
