f53a49e105d6829a5a6cd71f7c0be36ce60108dd
king19860907 <ludy19860907@163.com>
2016-03-23 00:12:48 +0800

    完善用户功能模块

--- src/test.js
+++ src/test.js
@@ -4,9 +4,23 @@
  * Created by jun_ma on 2016/3/22.
  */
 
-$.method('user.add').call({
+/*$.method('user.add').call({
     name : 'majun',
     email : '118109840@qq.com',
-    password:'123456'
+    password:'123456',
+    nickname:'雷小锋',
+    about:'穷玩车~富腕表~屌丝玩电脑',
 
+},console.log);*/
+
+/*
+$.method('user.get').call({
+    name:'majun',
+    email:'118109840@qq.com',
+},console.log);*/
+
+$.method('user.update').call({
+    name : 'majun',
+    email : '118109840@qq.com',
+    nickname : '雷小锋2',
 },console.log);
