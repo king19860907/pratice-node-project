b3ab6f5a322a3adbfa63d977fd89d5d29f6e6e89
king19860907 <ludy19860907@163.com>
2016-03-23 01:12:41 +0800

    用户模块api

---
+++ src/routes/login.js
@@ -0,0 +1,21 @@
+'use strict'
+/**
+ * Created by majun on 16/3/23.
+ */
+
+module.exports = function(done){
+
+    $.router.post('/api/login',async function(req,res,next){
+        
+    });
+
+    $.router.post('/api/logout',async function(req,res,next){
+
+    });
+
+    $.router.post('/api/signup',async function(req,res,next){
+
+    });
+
+    done();
+};
