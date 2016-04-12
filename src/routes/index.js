135eb731c97d39aac6288a53305c30fa8036bd04
jun_ma <ludy19860907@163.com>
2016-04-12 14:42:32 +0800

    增加nodemon

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@
 module.exports = function(done){
 
     $.router.get('/',function(req,res,next){
-        res.send(`现在是北京时间${new Date()}`);
+        res.send(`现在是北京时间${new Date()}111`);
         done();
     });
 }
