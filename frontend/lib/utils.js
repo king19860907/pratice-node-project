9515f6a13198eac1d9d6ba5b0a05675fb912dcc5
jun_ma <ludy19860907@163.com>
2016-04-26 15:52:03 +0800

    修复代码高亮问题

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -6,6 +6,13 @@ export function redirectUrl(url){
     location = url;
 }
 
+const xssOptions = {
+    whiteList: Object.assign({},xss.whiteList),
+}
+xssOptions.whiteList.code = ['class'];
+xssOptions.whiteList.span = ['class'];
+const myxss = new xss.FilterXSS(xssOptions);
+
 marked.setOptions({
     highlight: function (code) {
         return Highlight.highlightAuto(code).value;
@@ -13,5 +20,5 @@ marked.setOptions({
 });
 
 export function renderMarkdown(text){
-    return xss(marked(text));
+    return myxss.process(marked(text));
 }
